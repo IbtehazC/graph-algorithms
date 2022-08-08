db0c3fdfbbb16c70e2dc3ceba0322b3dcce6e45a
Ibtehaz Ali Chowdhury <ibtehaz.chowdhury@gmail.com>
2022-08-09 00:17:12 +0600

    binary tree implementation

--- sketch.js
+++ sketch.js
@@ -3,52 +3,9 @@ var tree;
 function setup() {
   noCanvas();
   tree = new Tree();
-  tree.addValue(5);
-  tree.addValue(3);
-  tree.addValue(7);
-  tree.addValue(6);
+  for (var i = 0; i < 10; i++) {
+    tree.addValue(floor(random(0, 100)));
+  }
   console.log(tree);
+  tree.traverse();
 }
-
-function draw() {
-  rect(10, 10, 50, 50);
-}
-
-function Tree() {
-  this.root = null;
-}
-
-Tree.prototype.addValue = function (val) {
-  var n = new Node(val);
-  if (this.root == null) {
-    this.root = n;
-  } else {
-    this.root.addNode(n);
-  }
-};
-
-Tree.prototype.traverse = function () {
-  this.root.visit();
-};
-
-function Node(val) {
-  this.value = val;
-  this.left = null;
-  this.right = null;
-}
-
-Node.prototype.addNode = function (n) {
-  if (n.value < this.value) {
-    if (this.left == null) {
-      this.left = n;
-    } else {
-      this.left.addNode(n);
-    }
-  } else if (n.value > this.value) {
-    if (this.right == null) {
-      this.right = n;
-    } else {
-      this.right.addNode(n);
-    }
-  }
-};
